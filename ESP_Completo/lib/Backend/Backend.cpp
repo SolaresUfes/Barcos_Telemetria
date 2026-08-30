404fae94e0ed255ad53e0efb274a701b1b6cbf3a
Augusto528 <65742088+Augusto528@users.noreply.github.com>
2026-08-30 10:26:51 -0300

    Preparando Backend.cpp pra .h

--- ESP_Completo/lib/Backend/Backend.cpp
+++ ESP_Completo/lib/Backend/Backend.cpp
@@ -53,11 +53,15 @@ const char *Url_celulas = "https://painel-f8r7.vercel.app/api/celulas";
 
 // ----------------- FUNCOES AUXILIARES -----------------
 
+// Usa o link para a inserção de DADOS no backend e insere os dados que vierem
 void enviar_dados_bateria(DADOS_BATERIA dado_bateria);
+// Usa o link para a inserção de ALERTAS no backend e insere os dados que vierem
 void enviar_alertas_bateria(ALERTAS_BATERIA alerta_bateria);
+// Usa o link para a inserção de VALORES DAS CELULAS no backend e insere os dados que vierem
 void enviar_dados_celulas(CELULAS_INDIVIDUAIS individuais);
+// Usa os alertas que vierem pra deixar no padrçao que o backend aceita e que fique mais fácil pra adicionar em outros lugares
 String retornar_alertas(ALERTAS_BATERIA alertas);
-
+// Conecta no Wi-fi que for compartilhado
 void conectar_wifi(const char *nome_rede, const char *senha);
 
 // ----------------- FUNÇÕES -----------------
