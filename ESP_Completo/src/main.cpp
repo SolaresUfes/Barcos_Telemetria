4b89884450499ddc8bde519061909bc1942dc069
Augusto528 <65742088+Augusto528@users.noreply.github.com>
2026-08-31 21:39:15 -0300

    Alterando main com Config.h

--- ESP_Completo/src/main.cpp
+++ ESP_Completo/src/main.cpp
@@ -8,51 +8,10 @@
 #include <ArduinoJson.h>      // Json
 #include <Adafruit_ADS1X15.h> // ADS
 
-// #define RXD2 16
-// #define TXD2 17
-// #define RS485_CONTROL 23
-
-#define SCL_ADS 22 // D22
-#define SDA_ADS 21 // D21
-
 #define ADR_ADS
 
 Adafruit_ADS1115 ads;
 
-// // ----------------- ESTRUTURAS DOS DADOS -----------------
-
-// // Estrutura com as variaveis dos dados do BMS
-// struct DADOS_BATERIA {
-//     float tensao;
-//     float corrente;
-//     float porcentagem;
-// };
-
-// // Estrutura com os valores (TRUE or FALSE) dos alertas do BMS
-// struct ALERTAS_BATERIA {
-//     bool celula_sobretensao;
-//     bool celula_subtensao;
-
-//     bool pack_sobretensao;
-//     bool pack_subtensao;
-
-//     bool temp_carga_alta;
-//     bool temp_carga_baixa;
-//     bool temp_descarga_alta;
-//     bool mosfet_temp_alta;
-
-//     bool corrente_carga_alta;
-//     bool corrente_descarga_alta;
-//     bool curto_circuito;
-//     bool mosfet_travado;
-
-//     bool alerta_ativo;
-// };
-
-// // Estrutura com dados individuais de tensao para cada celula
-// struct CELULAS_INDIVIDUAIS {
-//     float celulas[16] = {0};
-// };
 
 // ----------------- VARIAVEIS E CONSTANTES -----------------
 
@@ -100,8 +59,7 @@ String retornar_alertas(ALERTAS_BATERIA alertas);
 
 // ----------------- SETUP -----------------
 
-void setup()
-{
+void setup() {
 
     // Inicio do serial do monitor
     Serial.begin(115200);
