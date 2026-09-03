134f72b3a7e43ca649b2603d901ef5269c2c6b41
Augusto528 <65742088+Augusto528@users.noreply.github.com>
2026-09-03 16:50:34 -0300

    Teste com a main

--- ESP_Completo/src/main.cpp
+++ ESP_Completo/src/main.cpp
@@ -13,7 +13,6 @@
 unsigned long tempo_anterior = 0;
 const unsigned long intervalo_leitura = 2000;
 
-
 // ----------------- SETUP -----------------
 
 void setup() {
@@ -25,7 +24,7 @@ void setup() {
     iniciar_BMS();
 
     // Inicia o ADS e o barramento I2C
-    iniciar_ADS(SDA_ADS, SCL_ADS);
+    iniciar_ADS();
 
     // Conecta Wi-Fi
     conectar_wifi();
@@ -39,20 +38,21 @@ void loop() {
     // Executa a leitura a cada intervalo_leitura
     if (tempo_atual - tempo_anterior >= intervalo_leitura) {
 
-        tempo_anterior = tempo_atual;
+    //     tempo_anterior = tempo_atual;
 
-        // leitura dos dados gerais
-        DADOS_BATERIA BMS_dados = ler_dados_bms();
+    //     // leitura dos dados gerais
+    //     DADOS_BATERIA BMS_dados = ler_dados_bms();
 
-        // Envio dos dados
-        enviar_dados_bateria(BMS_dados);
+    //     // Envio dos dados
+    //     enviar_dados_bateria(BMS_dados);
 
-        // Alertas
-        ALERTAS_BATERIA BMS_alertas = ler_alertas_bms();
-        enviar_alertas_bateria(BMS_alertas);
+    //     // Alertas
+    //     ALERTAS_BATERIA BMS_alertas = ler_alertas_bms();
+    //     enviar_alertas_bateria(BMS_alertas);
 
-        // Células
-        CELULAS_INDIVIDUAIS BMS_celulas = ler_celulas_bms();
-        enviar_dados_celulas(BMS_celulas);
+    //     // Células
+    //     CELULAS_INDIVIDUAIS BMS_celulas = ler_celulas_bms();
+    //     enviar_dados_celulas(BMS_celulas);
     }
 }
+
